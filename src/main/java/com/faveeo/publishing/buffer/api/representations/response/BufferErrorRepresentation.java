82b169c069673b472617e7026d28acd2e2455430
Sylvain Leroy <sylvain@faveeo.com>
2019-02-07 16:37:13 +0100

    Adding missing property

--- src/main/java/com/faveeo/publishing/buffer/api/representations/response/BufferErrorRepresentation.java
+++ src/main/java/com/faveeo/publishing/buffer/api/representations/response/BufferErrorRepresentation.java
@@ -1,5 +1,6 @@
 package com.faveeo.publishing.buffer.api.representations.response;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.*;
 
 import java.util.List;
@@ -9,12 +10,14 @@ import java.util.List;
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
+@JsonIgnoreProperties
 public class BufferErrorRepresentation {
 
     public int code;
     public List<ProfileId> errored_profiles;
     public String message;
     public boolean success;
+    public String error;
 
     public static class ProfileId {
         public String profiled_id;
